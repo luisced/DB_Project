1329d02fd0cd03304fee89008763526994d80cea
luisced <luis.cedillo.maldonado@gmail.com>
2024-02-17 17:12:45 -0600

    feat: created top bar component

--- react_app/src/views/global/Topbar.jsx
+++ react_app/src/views/global/Topbar.jsx
@@ -1,7 +1,7 @@
 import { Box, IconButton, useTheme } from "@mui/material";
 import { useContext } from "react";
 import { ColorModeContext, tokens } from "../../theme";
-import InputBase from "@mui/material";
+import { InputBase } from "@mui/material";
 import LightModeOutlinedIcon from "@mui/icons-material/LightModeOutlined";
 import DarkModeOutlinedIcon from "@mui/icons-material/DarkModeOutlined";
 import NotificationsOutlinedIcon from "@mui/icons-material/NotificationsOutlined";
@@ -16,7 +16,13 @@ const Topbar = () => {
 	return (
 		<Box display="flex" justifyContent="space-between" p={2}>
 			{/* Search Bar */}
-			<Box display="flex" backgroundColor={colors.primary[400] borderRadius="3px"}></Box>
+			<Box
+				display="flex"
+				backgroundColor={colors.primary[400]}
+				borderRadius="3px"
+			>
+				<InputBase sx={{ ml: 2, flex: 1 }} placeholder="Search" />
+			</Box>
 		</Box>
 	);
 };
