d1a1ca42a8ef716a55b40d8e8bb349f7c3054b4f
luisced <luis.cedillo.maldonado@gmail.com>
2024-02-17 19:11:01 -0600

    fix: deleted box

--- react_app/src/App.js
+++ react_app/src/App.js
@@ -1,16 +1,41 @@
+import React, { useState } from "react";
 import { ColorModeContext, useMode } from "./theme";
 import { CssBaseline, ThemeProvider } from "@mui/material";
+import { Route, Routes } from "react-router-dom";
 import Topbar from "./views/global/Topbar";
+import Dashboard from "./views/dashboard";
+import CustomSidebar from "./views/global/Sidebar";
+// import Bar from "./views/bar";
+// import Form from "./views/Form";
+// import Line from "./views/Line";
+// import Pie from "./views/pie";
+// import FAQ from "./views/faq";
+// import Geography from "./views/geography";
 
 function App() {
 	const [theme, colorMode] = useMode();
+
 	return (
 		<ColorModeContext.Provider value={colorMode}>
 			<ThemeProvider theme={theme}>
 				<CssBaseline />
 				<div className="app">
+					<CustomSidebar />
 					<main className="content">
 						<Topbar />
+						<Routes>
+							<Route path="/" element={<Dashboard />} />
+							{/* <Route path="/team" element={<Team />} />
+              <Route path="/contacts" element={<Contacts />} />
+              <Route path="/invoices" element={<Invoices />} />
+              <Route path="/form" element={<Form />} />
+              <Route path="/bar" element={<Bar />} />
+              <Route path="/pie" element={<Pie />} />
+              <Route path="/line" element={<Line />} />
+              <Route path="/faq" element={<FAQ />} />
+              <Route path="/calendar" element={<Calendar />} />
+              <Route path="/geography" element={<Geography />} /> */}
+						</Routes>
 					</main>
 				</div>
 			</ThemeProvider>
