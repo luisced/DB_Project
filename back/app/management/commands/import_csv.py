6c488ccd6d00a27155d7b8b81f531732ff9ee43b
Javier Rangel <0256158@up.edu.mx>
2024-04-22 15:36:11 -0600

    Working DB

--- back/app/management/commands/import_csv.py
+++ back/app/management/commands/import_csv.py
@@ -13,93 +13,104 @@ user_cache = {}
 device_cache = {}
 geo_location_cache = {}
 
-def get_or_create_user(name_data):
-    if name_data not in user_cache:
-        user_cache[name_data]=name_data
-        query = AfectedUser.objects.filter(username=name_data)
-        if query.exists():
-            user_cache[name_data] = query.first()
-        else:
-            user = AfectedUser.objects.create(username=name_data)
-        return user
-    return user_cache[name_data]
+def get_or_create_user(username):
+    if username not in user_cache:
+        user, created = AfectedUser.objects.get_or_create(username=username)
+        user_cache[username] = user
+    return user_cache[username]
+
 
 def deviceSplit(device_info):
-    #Before ( = web_browser
-    #In () = operative_system
-    #After ) = rest_information
-    
-    web_browser = device_info.split(' (')[0]
-    os = device_info.split(' (')[1].split(')')[0]
-    rest = device_info.split(') ')[1]
-    return web_browser, os, rest
+    try:
+        '''
+        Before the first ( found = web_browser
+        After the first ( found and before the first ) found = os
+        After the first ) found = rest
+        '''
+        web_browser = device_info.split('(')[0].strip()
+        os = device_info.split('(')[1].split(')')[0].strip()
+        rest = device_info.split(')')[1].strip()
+        return web_browser, os, rest
+    except IndexError:
+        return device_info, None, None
 
 def get_or_create_device(device_info):
     if device_info not in device_cache:
         web_browser, os, rest = deviceSplit(device_info)
-        device_cache[device_info][web_browser]=web_browser
-        device_cache[device_info][os]=os
-        device_cache[device_info][rest]=rest
-        query = Device.objects.filter(web_browser=web_browser, os=os, rest=rest)
-        if query.exists():
-            device_cache[device_info] = query.first()
-        else:
-            device = Device.objects.create(device=device_info, web_browser=web_browser, os=os, rest=rest)
-        return device
+        device, created = Device.objects.get_or_create(
+            web_browser=web_browser, 
+            operative_system=os, 
+            rest_information=rest
+        )
+        device_cache[device_info] = device
     return device_cache[device_info]
+
 
 def get_or_create_geolocation(location_data):
     if location_data not in geo_location_cache:
-        query = Geolocalization.objects.filter(location=location_data)
-        if query.exists():
-            geo_location_cache[location_data] = query.first()
-        else:
-            locality , city = location_data.split(', ')
-            geo_location = Geolocalization.objects.create(location=location_data, locality=locality, city=city)
+        try:
+            locality, city = location_data.split(', ')
+        except ValueError:
+            locality = location_data
+            city = ''
+        geo_location, created = Geolocalization.objects.get_or_create(
+            locality=locality, 
+            city=city
+        )
         geo_location_cache[location_data] = geo_location
+
     return geo_location_cache[location_data]
 
 
+
 def process_chunk(chunk_data):
+    from django import db
+    db.connections.close_all()  # Close existing database connections
     import django
     django.setup()
 
     cyber_attacks_to_create = []
+    print(f"Processing chunk of {len(chunk_data)} records...")
 
-    for _, row in chunk_data.iterrows():
-        naive_timestamp = datetime.strptime(row['Timestamp'], '%Y-%m-%d %H:%M:%S')
-        aware_timestamp = make_aware(naive_timestamp)
+    try:
+        for _, row in chunk_data.iterrows():
+            naive_timestamp = datetime.strptime(row['Timestamp'], '%Y-%m-%d %H:%M:%S')
+            aware_timestamp = make_aware(naive_timestamp)
 
-        user = get_or_create_user(row['User Information'])
-        device = get_or_create_device(row['Device Information'])
-        geo_location = get_or_create_geolocation(row['Geo-location Data'])
-        
-        cyber_attack = CyberAttack(
-            timestamp=aware_timestamp,
-            sourceIP=row['Source IP Address'],
-            destinationIP=row['Destination IP Address'],
-            sourcePort=row['Source Port'],
-            destinationPort=row['Destination Port'],
-            protocol=row['Protocol'],
-            packetLength=row['Packet Length'],
-            packetType=row['Packet Type'],
-            trafficType=row['Traffic Type'],
-            actionTaken=row['Action Taken'],
-            severityLevel=row['Severity Level'],
-            networkSegment=row['Network Segment'],
-            alertsWarnings=row['Alerts/Warnings']=="Alert Triggered",
-            attackType=row['Attack Type'],
-            idsIpsAlerts=row['IDS/IPS Alerts']=="Alert Data",
-            user=user,
-            device=device,
-            geoLocation=geo_location
-        )
-        cyber_attacks_to_create.append(cyber_attack)
+            user = get_or_create_user(row['User Information'])
+            device = get_or_create_device(row['Device Information'])
+            geo_location = get_or_create_geolocation(row['Geo-location Data'])
+            cyber_attack = CyberAttack(
+                timestamp=aware_timestamp,
+                sourceIP=row['Source IP Address'],
+                destinationIP=row['Destination IP Address'],
+                sourcePort=row['Source Port'],
+                destinationPort=row['Destination Port'],
+                protocol=row['Protocol'],
+                packetLength=row['Packet Length'],
+                packetType=row['Packet Type'],
+                trafficType=row['Traffic Type'],
+                actionTaken=row['Action Taken'],
+                severityLevel=row['Severity Level'],
+                networkSegment=row['Network Segment'],
+                alertsWarnings=row['Alerts/Warnings']=="Alert Triggered",
+                attackType=row['Attack Type'],
+                idsIpsAlerts=row['IDS/IPS Alerts']=="Alert Data",
+                user=user,
+                device=device,
+                geoLocation=geo_location
+            )
+            cyber_attacks_to_create.append(cyber_attack)
 
-    # Bulk insert
-    CyberAttack.objects.bulk_create(cyber_attacks_to_create, batch_size=500)
+        CyberAttack.objects.bulk_create(cyber_attacks_to_create, batch_size=500)
+        print(f"Processed {len(cyber_attacks_to_create)} records.")
+    except Exception as e:
+        # Log the exception, consider re-raising or handling it as needed
+        print(f"Error processing chunk: {e}")
+        return 0
 
     return len(cyber_attacks_to_create)
+
 
 class Command(BaseCommand):
     help = 'Import CSV data into the database using multiprocessing for efficiency.'
